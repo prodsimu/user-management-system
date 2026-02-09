068f0d6323a09fd2d041773f34c4f51a2ed8f832
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-02-09 20:36:25 -0300

    feat: add User model with core attributes and methods

---
+++ sistem/main.py
@@ -0,0 +1,6 @@
+from user import *
+
+user_test = User(1, "jose da silva", "josedasilva",
+                 "123456", "user", True, 0)
+
+print(user_test.username)
---
+++ sistem/user.py
@@ -0,0 +1,35 @@
+class User:
+    def __init__(self,
+                 id, name, username, password,
+                 role, active, login_attempts):
+        self.id = id
+        self.name = name
+        self. username = username
+        self.password = password
+        self.role = role
+        self.active = active
+        self. login_attempts = login_attempts
+
+
+    def verify_password(self, password):
+        return self.password == password
+
+
+    def change_password(self, new_password):
+        self.password = new_password
+
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+
+    def deactivate(self):
+        self.active = False
+
+
+    def to_dict(self):
+        pass
