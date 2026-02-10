1fda69630965f3cfabec0f11894998241fffd000
simu-T14 <139299982+prodsimu@users.noreply.github.com>
2026-02-10 16:17:10 -0300

    feat: add update_user method

--- system/repositories/user_repository.py
+++ system/repositories/user_repository.py
@@ -40,9 +40,26 @@ class UserRepository:
     
 
     def delete_user(self, user_id: int):
-        for user in self.database.users:
+        users = self.database.users
+
+        for i, user in enumerate(users):
             if user.id == user_id:
-                self.database.users.remove(user)
+                users.pop(i)
                 return True
-            
+
         return False
+    
+
+    def update_user(self, user_id: int, new_username: str = None, new_password: str = None):
+        user = self.get_user_by_id(user_id)
+
+        if not user:
+            return False
+
+        if new_username is not None:
+            user.username = new_username
+
+        if new_password is not None:
+            user.password = new_password
+
+        return True
