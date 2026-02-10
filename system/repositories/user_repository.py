02dbe40e468c4f1a0d3a16e43a915c97ac56e8cb
simu-T14 <139299982+prodsimu@users.noreply.github.com>
2026-02-10 13:04:24 -0300

    feat: add get_next_id method

--- system/repositories/user_repository.py
+++ system/repositories/user_repository.py
@@ -13,3 +13,14 @@ class UserRepository:
 
     def get_all_users(self):
         return self.database.users
+
+
+    def get_next_id(self):
+        if not self.database.users:
+            return 1
+        
+        last_id = self.database.users[-1].id
+        return last_id + 1
+    
+
+    
