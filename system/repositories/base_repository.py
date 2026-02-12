f2a4a5f2a8a56b698f41a2a7ab834e29c6020ec8
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-02-12 20:43:22 -0300

    refactor: group BaseRepository methods by CRUD sections

--- system/repositories/base_repository.py
+++ system/repositories/base_repository.py
@@ -4,16 +4,14 @@ from database.database import Database
 class BaseRepository:
 
     def __init__(self, database: Database, collection_name: str):
-        self.database = database
-        self.collection = getattr(database, collection_name)
+            self.database = database
+            self.collection = getattr(database, collection_name)
 
+
+#  CREATE
 
     def add(self, item):
         self.collection.append(item)
-
-
-    def get_all(self):
-        return self.collection
 
 
     def get_next_id(self):
@@ -24,6 +22,12 @@ class BaseRepository:
         return last_id + 1
 
 
+# READ
+
+    def get_all(self):
+        return self.collection
+
+
     def get_by_id(self, item_id: int):
         for item in self.collection:
             if item.id == item_id:
@@ -32,10 +36,27 @@ class BaseRepository:
         return None
 
 
-    def delete(self, item_id: int):
-        for i, item in enumerate(self.collection):
-            if item.id == item_id:
-                self.collection.pop(i)
+    def get_by_field(self, field: str, value):
+        for item in self.collection:
+
+            attr_value = getattr(item, field, None)
+
+            if attr_value == value:
+                return item
+            
+        return None
+
+
+    def exists_by_id(self, item_id: int):
+        return any(item.id == item_id for item in self.collection)
+
+
+    def exists_by_field(self, field: str, value):
+        for item in self.collection:
+
+            attr_value = getattr(item, field, None)
+
+            if attr_value == value:
                 return True
             
         return False
@@ -45,9 +66,7 @@ class BaseRepository:
         return len(self.collection)
 
 
-    def exists_by_id(self, item_id: int):
-        return any(item.id == item_id for item in self.collection)
-
+# UPDATE
 
     def update_by_id(self, item_id: int, new_data: dict):
         for item in self.collection:
@@ -66,23 +85,12 @@ class BaseRepository:
         return False
 
 
-    def get_by_field(self, field: str, value):
-        for item in self.collection:
+# DELETE
 
-            attr_value = getattr(item, field, None)
-
-            if attr_value == value:
-                return item
-            
-        return None
-
-
-    def exists_by_field(self, field: str, value):
-        for item in self.collection:
-
-            attr_value = getattr(item, field, None)
-
-            if attr_value == value:
+    def delete(self, item_id: int):
+        for i, item in enumerate(self.collection):
+            if item.id == item_id:
+                self.collection.pop(i)
                 return True
             
         return False
