c2987f7cc9a84669caaf05c4c72ec67f6ed46439
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-02-12 19:19:06 -0300

    refactor: remove redundant username methods using base repository generics

--- system/repositories/user_repository.py
+++ system/repositories/user_repository.py
@@ -6,15 +6,3 @@ class UserRepository(BaseRepository):
 
     def __init__(self, database: Database):
         super().__init__(database, "users")
-
-
-    def get_user_by_username(self, username: str):
-        for user in self.collection:
-            if user.username == username:
-                return user
-            
-        return None
-    
-
-    def exists_by_username(self, username: str):
-        return any(user.username == username for user in self.collection)
