3c9d1f7804107658b8b810d6aab30ed67fc84ded
simu-T14 <139299982+prodsimu@users.noreply.github.com>
2026-02-13 10:22:13 -0300

    feat: implement user creation logic

--- system/services/user_service.py
+++ system/services/user_service.py
@@ -7,6 +7,21 @@ class UserService:
         self.user_repository = user_repository
 
 
-    def create_user(self, name, username, password):
-        
-        pass
+    def create_user(self, new_name, new_username, new_password):
+
+        if self.user_repository.exists_by_field("username", new_username):
+            return False
+
+        new_user = User(
+            id = self.user_repository.get_next_id(),
+            name = new_name,
+            username = new_username,
+            password = new_password,
+            role = "user",
+            active = True,
+            login_attempts = 0
+        )
+
+        self.user_repository.add(new_user)
+
+        return new_user
