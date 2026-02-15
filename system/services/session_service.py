d0172682e69eba45fe29f2b60b802f3a10ac3fb1
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-02-15 20:05:05 -0300

    feat: implement session creation in SessionService

--- system/services/session_service.py
+++ system/services/session_service.py
@@ -1,7 +1,14 @@
 from repositories.session_repository import SessionRepository
+from models.session import Session
 
 
 class SessionService:
 
     def __init__(self, session_repository: SessionRepository):
         self.session_repository = session_repository
+
+    def create_session(self, user_id: int) -> None:
+        new_session = Session(user_id)
+        self.session_repository.add(new_session)
+
+        return new_session
