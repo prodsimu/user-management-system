d65208aea5d4d7e904f629f569f1d9dfedf92011
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-02-16 18:17:04 -0300

    refactor: reorganize UI folder structure

--- system/main.py
+++ system/main.py
@@ -3,10 +3,11 @@ from repositories.session_repository import SessionRepository
 from repositories.user_repository import UserRepository
 from services.session_service import SessionService
 from services.user_service import UserService
-from ui.menu import Menu
+from ui.menu import Output
 
 
 def main():
+    system_is_starting = None
     current_user = None
     current_session = None
 
@@ -18,14 +19,20 @@ def main():
     session_service = SessionService(session_repository)
     user_service = UserService(user_repository, session_service)
 
-    menu = Menu(user_service, session_service)
+    menu_output = Output(user_service, session_service)
 
     if not user_service.list_users():
         initial_admin = user_service.create_user("admin", "admin", "admin123")
         user_service.change_role_by_id(initial_admin.id, "admin")
+        system_is_starting = True
 
     while True:
-        pass
+
+        if system_is_starting:
+            menu_output.start_system()
+            pass
+
+        break
 
 
 if __name__ == "__main__":
--- system/ui/menu.py
+++ system/ui/menu.py
@@ -1,11 +1,29 @@
-from typing import Optional, Tuple
+from typing import Any
 from models.user import User
 from models.session import Session
 from services.user_service import UserService
 from services.session_service import SessionService
 
 
-class Menu:
+class Output:
 
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        user_service: UserService,
+        session_service: SessionService,
+    ) -> None:
+        self.user_service: UserService = user_service
+        self.session_service: SessionService = session_service
+
+    def start_system(self, seed: dict[str, Any]) -> None:
+        print(
+            "That's the first system initialization\n"
+            + "The system will create a initial admin user\n"
+        )
+        print(f"Name = {seed.name}")
+        print(f"Username = {seed.username}")
+        print(f"Password = {seed.password}")
+
+
+class input:
+    pass
