d35e617c9cec877569401e3545bf1d5a3bd16a82
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-02-16 11:47:17 -0300

    refactor: improve database typing

--- system/database/database.py
+++ system/database/database.py
@@ -1,4 +1,9 @@
+from typing import List
+from models.user import User
+from models.session import Session
+
+
 class Database:
-    def __init__(self):
-        self.users = []
-        self.sessions = []
+    def __init__(self) -> None:
+        self.users: List[User] = []
+        self.sessions: List[Session] = []
